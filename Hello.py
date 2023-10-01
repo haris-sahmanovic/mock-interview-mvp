c6675cb5efa781be5c345855e6ca130af9211223
haris-sahmanovic <131079368+haris-sahmanovic@users.noreply.github.com>
2023-10-01 19:55:37 +0000

    Initial commit

--- Hello.py
+++ Hello.py
@@ -14,37 +14,115 @@
 
 import streamlit as st
 from streamlit.logger import get_logger
+import base64
+import openai
+
+
+def read_txt_file(file_path):
+    with open(file_path, 'r') as file:
+        return file.read()
 
 LOGGER = get_logger(__name__)
 
+try:
+    openai.api_key = "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"
+except KeyError:
+    st.write("Error: The OPENAI_API_KEY environment variable is not set.")
+    st.stop()
+
 
 def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
-
-    st.write("# Welcome to Streamlit! 👋")
-
-    st.sidebar.success("Select a demo above.")
-
+    # Streamlit UI
+    # Center the image using HTML
     st.markdown(
         """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
+        <div style="text-align: center;">
+            <img src="data:image/jpg;base64,{}" style="max-width: 100%;">
+        </div>
+        """.format(
+            base64.b64encode(open("aiphoto.jpeg", "rb").read()).decode()
+        ),
+        unsafe_allow_html=True,
     )
+    st.title('Welcome to Your AI Mock Interview!')
+    st.write(' ')
+    with st.expander("📘 Instructions", expanded=False):
+        st.write(' ')
+        st.write("This AI is designed to simulate technical interviews, offering you a valuable practice experience. Feel "
+                "free to ask for clarifications or constraints, just as you would in a real-life interview")
+        st.write(' ')
+        st.write("For a more authentic experience, we recommend using your computer's speech-to-text feature rather than "
+                "typing. Much of interview preparation involves becoming comfortable with verbalizing your thoughts. "
+                "Good luck!'")
+
+
+    # Add a horizontal line or divider
+    st.markdown("<hr/>", unsafe_allow_html=True)
+
+    if "openai_model" not in st.session_state:
+        st.session_state["openai_model"] = "gpt-4"
+        # st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    question_number = st.selectbox(
+        "Select Question Number:",
+        (1, 2, 3)
+    )
+    st.write(' ')
+
+    txt_data = read_txt_file(f"interview_questions/question{question_number}.txt")
+
+    if not any(msg['role'] == 'system' for msg in st.session_state.messages):
+      st.session_state.messages.append({"role": "system",
+                                        "content": "You are a 27 year old software engineer working for Google conducting a technical interview to a potential candidate."
+                                      "Try to act personable and relaxed. Do not say you are an AI or anything unnatural for an interview."
+                                      "You are not to directly assist them or giveaway any answers or serious insights. You are to judge them"
+                                      "on how well they can think about and answer the question."
+                                      "With that said, if you notice them struggling offer hints that are given to you in the following text,"
+                                      "or try to veer them to the right direction, but DO NOT give away the answers."
+                                      "Furthermore, if they offer a solution that is not listed as a valid solution in the given text,"
+                                      "make sure its not listed in the interview_questions pitfalls. If it isnt in the pitfalls, please let them know you're not sure about it or thats not what we're really looking for."
+                                      "Lastly, consider starting the interview with a casual converstation and asking about their previous work experience,"
+                                      "general career info etc. Dont be afraid to ask about more info such as what they do at [candidates current company], or anything similar"
+                                      "Lastly, when a user types FINISH, the interview is over. Tell them how they did and be honest - don't be too nice or worry about their feelings."
+                                      ""
+                                      "Please ask the following question and evaluate how well candidate does"
+                                      f"{txt_data}"
+                                      })
+
+
+    # Display messages except the system message
+    for message in st.session_state.messages:
+        if message["role"] != "system":  # Skip system message for display
+            with st.chat_message(message["role"]):
+                st.markdown(message["content"])
+
+    if prompt := st.chat_input("Talk to your interviewer"):
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.markdown(prompt)
+
+        with st.chat_message("assistant"):
+            message_placeholder = st.empty()
+            full_response = ""
+            for response in openai.ChatCompletion.create(
+                model=st.session_state["openai_model"],
+                messages=[
+                    {"role": m["role"], "content": m["content"]}
+                    for m in st.session_state.messages
+                ],
+                stream=True,
+            ):
+                full_response += response.choices[0].delta.get("content", "")
+                message_placeholder.markdown(full_response + "▌")
+            message_placeholder.markdown(full_response)
+        st.session_state.messages.append({"role": "assistant", "content": full_response})
+
+
+
+
 
 
 if __name__ == "__main__":
